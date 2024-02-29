82f9610fc3929117dd2e0b00515c3830b27e3282
Vithor <104462358+VHMR3000@users.noreply.github.com>
2024-02-29 14:41:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 conts botoes = document.querySelectorAll (".botao");
+const textos=  document.querySelectorAll(".aba-conteudo")
 
 for(let i=0; <botoes.lenght;i++) {
     botoes[i] .onlick = function() {
         for(let j=0;<botoes.lenght;j++) {
             botoes[j].classList.remove("ativo");
         }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo")
     }
+}
+
+const contadores = document.quer.querySelectorAll(".contador");
+const contadores= new date("2024-10-05T00-00-00")
+let tempoatual = new date
+;
+contadores[0].textContent = calculatempo(tempoobjetivo);
+
+function calculatempo(tempoobjetivo) {
+    let tempofinal= tempoobjetivo - tempoatual;
+    let segundos = tempofinal/1000;
+    return segundos;
 }
